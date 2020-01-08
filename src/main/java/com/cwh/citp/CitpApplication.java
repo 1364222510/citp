5f7d2090fe5d077a74d4ccadb6200679deaa9705
1364222510 <2391504064@qq.com>
2020-01-08 15:38:16 +0800

    初始化版本

---
+++ src/main/java/com/cwh/citp/CitpApplication.java
@@ -0,0 +1,13 @@
+package com.cwh.citp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cwh/citp/CitpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwh.citp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
